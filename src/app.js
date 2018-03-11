a36341b9efb3b3e134b5a9869643687cc3fe0be5
Mike Streltsoff <MStreltsoff@gmail.com>
2018-03-10 19:12:23 -0500

    refactor for function and easier readability

--- src/app.js
+++ src/app.js
@@ -1,12 +1,52 @@
 const express = require('express');
-
-
-const pricesController = require('./controllers/prices.js');
-
+const bodyParser = require('body-parser');
 const app = express();
-const PORT = 3000
 
-app.use(pricesController);
+
+const fetch = require('node-fetch');
+
+const PORT = 3000
+const STATUS_ERROR = 422
+const STATUS_SUCCESS = 200
+
+
+const API_CURRENT = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+const API_YESTERDAY = 'https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday';
+
+
+app.use(bodyParser.json());
+
+
+let CURRENT_PRICE = {};
+let YESTERDAY_PRICE = {}
+
+
+app.get('/current' , (req, res) => {
+    fetch(API_CURRENT)
+    .then(res => res.json())
+    .then(json => {
+        res.status(STATUS_SUCCESS);
+        res.json(json);
+    })
+    .catch(err => {
+        res.status(STATUS_ERROR);
+        res.json({ error: err });
+      });
+  });
+
+app.get('/yesterday', (req, res) => {
+    fetch(API_YESTERDAY)
+    .then(res => res.json())
+    .then(json => {
+        res.status(STATUS_SUCCESS);
+      res.json(json);
+    })
+    .catch(err => {
+      res.status(STATUS_ERROR);
+      res.json({ error: err });
+    });
+});
+
 
 app.listen(PORT, err => {
   if (err) {
